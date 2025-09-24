8e5ce5e3cacab1acc15eb8449d0a6dfc6911d0df
Thiago Yashimura Torres <thiagoyashimura03@gmail.com>
2025-09-24 20:08:55 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ function App() {
   const [contador, setContador] = React.useState(0)
 
   function aumentar(){
-    setContador( prevC => prevC + 1)
+    setContador( prevC => prevC + 5)
   }
   function diminuir(){
-    setContador ( prevC => prevC - 1)
+    setContador ( prevC => prevC - 5)
   }
   function reset(){
     setContador ( prevC => prevC = 0)
   }
     return (
     <main>
-      <h1 className='titulo'>Contador <span>React</span></h1>
+      <h1 className='titulo'>Contador <span className='t'>React</span></h1>
       
     <section className="container">
       <div className='marks'>
-            <div className='mark'></div>
-            <div className='mark'></div>
-            <div className='mark'></div>
+            <div className='mark'>
+              <span className='m'>25</span>
+              <span className={contador >= 25 ? 'meta' : 'none'}>{ contador >= 25 ? '.' : ''}</span>
+            </div>
+            <div className='mark'>
+              <span className='m'>50</span>
+              <span className={contador >= 50 ? 'meta' : 'none'}>{ contador >= 50 ? '.' : '' }</span>
+            </div>
+            <div className='mark'>
+              <span className='m'>100</span>
+              <span className={contador >= 100 ? 'meta' : 'none'}>{ contador >= 100 ? '.' : '' }</span>
+            </div>
       </div>
             <div className="counter">
               
